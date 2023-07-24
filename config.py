9050a7711a5d11a8967e2ef432d12b65154795d7
Gift Clement Abogha <cgift827@gmail.com>
2023-07-24 13:33:42 +0000

    i am adding the neccessary settings for the shop project

--- config.py
+++ config.py
@@ -1,4 +1,25 @@
+import os
+import pymysql
+
+
 
 
 class Config:
+    DEBUG = 0
+    SECRET_KEY=os.environ.get('SECRET_KEY')
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = os.environ.get('MAIL_PORT')
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+
+
+class DevelopmentConfig(Config):
     DEBUG = 1
+    SECRET_KEY=os.environ.get('SECRET_KEY')
+
+config = {
+    'development':DevelopmentConfig,
+    'production':ProductionConfig,
+    'testing':TestingConfig,
+    'default':DevelopmentConfig
+}
