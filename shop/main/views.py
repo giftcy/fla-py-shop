36c5885256b2f77e173139e24f1fe3a512046af3
Gift Clement Abogha <cgift827@gmail.com>
2023-07-27 11:33:20 +0000

    i just added the page layout for the site

--- shop/main/views.py
+++ shop/main/views.py
@@ -1,13 +1,14 @@
 from . import main
+from flask import render_template
 
 
 @main.route("/home")
 @main.route("/")
 def index():
-    return 'home'
+    return render_template('main/index.html')
 
 
 @main.route("/about-us")
 @main.route("/about")
 def about():
-    return 'about'
+    return render_template('main/about.html')
